18f4693d0ec221209ac6196c394e40c772ced98b
Candy <candyLyl687423@126.com>
2024-08-23 17:16:56 +0800

    Initial

---
+++ src/main/java/com/buaa/werweruser/WerwerUserApplication.java
@@ -0,0 +1,13 @@
+package com.buaa.werweruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerwerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerwerUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buaa/werweruser/WerwerUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buaa.werweruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerwerUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
